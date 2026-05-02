e1e0ed6be953c48f27c056507a9f09ae49d9595b
EricThunder <67475344+EricThunder@users.noreply.github.com>
2026-05-02 10:38:46 -0400

    message

--- main.py
+++ main.py
@@ -9,22 +9,25 @@ window = pygame.display.set_mode((constants.window_width, constants.window_heigh
 surface = pygame.Surface([constants.window_width, constants.window_height], pygame.SRCALPHA, 32)
 
 def draw_background(window):
-    window.fill((255, 0, 0))
+    surface.fill((20, 20, 20))
     # Keep your grid lines if you want them on top:
-    spacing = 60
+    spacing = 25
     for x in range(0, constants.window_width, spacing):
         pygame.draw.line(window, (255, 255, 255), (x, 0), (x, constants.window_height))
     for y in range(0, constants.window_height, spacing):
-        pygame.draw.line(window, (18, 18, 28), (0, y), (constants.window_width, y))
+        pygame.draw.line(window, (255, 255, 255), (0, y), (constants.window_width, y))
 
 running = True
 while running:
     draw_background(window)
 
-    # press q to quit the program
+    # quit the program
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
 
+    # draw to objects
+    window.blit(surface, (0,0))
+    
     # continuously redraw the screen
     pygame.display.flip()
