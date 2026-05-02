b45cf7aaedd0d67113ad1ef53aabd4d2e3dcf014
SaffronWare <aryanshadzi@gmail.com>
2026-05-02 10:50:37 -0400

    added generic bridge creator

--- bridge_creator.py
+++ bridge_creator.py
@@ -2,13 +2,26 @@ from constants import *
 import pygame 
 from particle import Particle
 from connection import Connection
+from bridge import Bridge
+from copy import deepcopy
 
 class BridgeCreator:
-    def __init__(self, path, num_rods):
-        self.start = path[0]
-        self.end = path[1]
-        self.roads = path
+    @staticmethod
+    def generate_generic(self, path):
+    
         
         # for now generic algorithm
-
+        particles = deepcopy(path)
+        connections = []
+        for p1,p2 in zip(particles[:-1], particles[1:]):
+            curr_connection = Connection(p1,p2)
+            connections.append(curr_connection)
+        
+        generated_bridge = Bridge()
+        generated_bridge.particles = particles
+        generated_bridge.connections = connections
+        
+        return generated_bridge
+        
+    
 
